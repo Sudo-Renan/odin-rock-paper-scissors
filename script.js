30cd75e37be59dd7026a9c93e614cedd774b6203
Renan Reis <82418573+Sudo-Renan@users.noreply.github.com>
2023-02-18 13:17:41 -0300

    Update the script.js adding the function jogo and the getUserChoise

--- script.js
+++ script.js
@@ -9,21 +9,39 @@ function getRandomInt(max) {
 
 //   function to make the script take an random number
 function getComputerChoice() {
-    let beholder = getRandomInt(2)
+    let beholder = getRandomInt(3)
+    let result = ''
 
     switch (beholder) {
         case 0:
+            result = 'ROCK'
             console.log("Rock")
             break;
 
         case 1:
+            result = 'PAPER'
             console.log("Paper")
             break;
 
         case 2:
+            result = 'SCISSORS'
             console.log("Scissors")
             break;
     }
+    return result   
 }
 
-getComputerChoice()
+function getUserChoice(){
+    let result = 'PAPER'
+    return result
+}
+
+function jogo(){
+    let pc = getComputerChoice(), user =  getUserChoice()
+   
+    if((pc == 'ROCK' && user != 'PAPER') || (pc == 'PAPER' && user != 'SCISSORS') || (pc == 'SCISSORS' && user != 'ROCk')){
+        console.log('COMPUTADOR GANHOU')
+        
+    } else console.log('USER GANHOU')
+}
+jogo()
